ac2752a04c87468aa860a79022f9d235a6c2f6fa
MortenBirk <mortenhbirk@gmail.com>
2014-11-28 13:56:17 +0100

    Add accelerometer activity. This only outputs the data and don't write to a file

---
+++ app/src/main/java/com/cac/camp/camp/LogAccActivity.java
@@ -0,0 +1,87 @@
+package com.cac.camp.camp;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+
+
+public class LogAccActivity extends Activity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_acc);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new LogAccFragment())
+                    .commit();
+        }
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //Right in here is where you put code to read the current sensor values and
+        //update any views you might have that are displaying the sensor information
+        //You'd get accelerometer values like this:
+        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
+            return;
+        }
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        TextView text = (TextView)findViewById(R.id.showAcc);
+        text.setText("x: " + Float.toString(x) + "\n" + "y: " + Float.toString(y) + "\n" + "z: " + Float.toString(z));
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class LogAccFragment extends Fragment {
+
+        public LogAccFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_log_acc, container, false);
+            return rootView;
+        }
+    }
+}
--- app/src/main/java/com/cac/camp/camp/StartScreenActivity.java
+++ app/src/main/java/com/cac/camp/camp/StartScreenActivity.java
@@ -3,6 +3,7 @@ package com.cac.camp.camp;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,7 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-
 
 
 public class StartScreenActivity extends Activity {
@@ -21,37 +21,23 @@ public class StartScreenActivity extends Activity {
         setContentView(R.layout.activity_start_screen);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new StartViewFragment())
                     .commit();
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start_screen, menu);
-        return true;
+    public void runLogAcc(View view) {
+        Intent intent = new Intent(this, LogAccActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class StartViewFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public StartViewFragment() {
         }
 
         @Override
