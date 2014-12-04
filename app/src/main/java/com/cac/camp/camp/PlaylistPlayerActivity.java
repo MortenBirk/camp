874715b9184d7ef45566ab12a22e1f7e67fd61d7
Nikolai Øllegaard <nikolai@oellegaard.com>
2014-12-04 13:56:54 +0100

    add more playability

--- app/src/main/java/com/cac/camp/camp/PlaylistPlayerActivity.java
+++ app/src/main/java/com/cac/camp/camp/PlaylistPlayerActivity.java
@@ -1,17 +1,85 @@
 package com.cac.camp.camp;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.MediaController;
+
+import com.spotify.sdk.android.playback.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PlaylistPlayerActivity extends Activity {
+    Player mPlayer;
+    List<String> playlist = new ArrayList<String>();
+    ListView playlistView;
+    Boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist_player);
+
+        playlistView = (ListView) findViewById(R.id.playlistView);
+
+        playlist.add("spotify:track:5QruhkvTtEi6cR8nSr6LwT");
+        playlist.add("spotify:track:4KqQfcx8RM9liPiHEvEJNg");
+        playlist.add("spotify:track:768j11Y0ksbgdncnFuAEZ4");
+        playlist.add("spotify:track:3DqhtDaWayCtfV69d2p5vH");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getBaseContext(),
+                android.R.layout.simple_list_item_1,
+                playlist
+        );
+
+        playlistView.setAdapter(adapter);
+
+        mPlayer = SpotifySingleton.getInstance().getPlayer();
+
+        if (isPlaying == false) {
+            mPlayer.play(playlist);
+            isPlaying = true;
+        }
+
+        Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+        Button playbutton = (Button) findViewById(R.id.playbutton);
+        Button nextbutton = (Button) findViewById(R.id.nextbutton);
+        Button previousbutton = (Button) findViewById(R.id.previousbutton);
+        playbutton.setTypeface(font);
+        nextbutton.setTypeface(font);
+        previousbutton.setTypeface(font);
+
+    }
+
+    public void play(View view){
+        Button playbutton = (Button) findViewById(R.id.playbutton);
+        if (isPlaying) {
+            mPlayer.pause();
+            playbutton.setText(R.string.fa_pause);
+
+        } else {
+            mPlayer.resume();
+            playbutton.setText(R.string.fa_play);
+        }
+        isPlaying = !isPlaying;
+    }
+
+    public void next(View view){
+        mPlayer.skipToNext();
+    }
+
+    public void previous(View view) {
+        mPlayer.skipToPrevious();
     }
 
 
--- app/src/main/java/com/cac/camp/camp/SpotifySingleton.java
+++ app/src/main/java/com/cac/camp/camp/SpotifySingleton.java
@@ -1,5 +1,7 @@
 package com.cac.camp.camp;
 
+import com.spotify.sdk.android.playback.Player;
+
 /**
  * Created by nikolaiollegaard on 03/12/14.
  */
@@ -9,7 +11,16 @@ public class SpotifySingleton {
     public static SpotifySingleton getInstance() {
         return ourInstance;
     }
+    private Player mPlayer;
 
     private SpotifySingleton() {
+    }
+
+    public Player getPlayer() {
+        return mPlayer;
+    }
+
+    public void setPlayer(Player mPlayer) {
+        this.mPlayer = mPlayer;
     }
 }
