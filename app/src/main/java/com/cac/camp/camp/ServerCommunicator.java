311a1d221eda97add84bfa0a3b100e93d758426e
MortenBirk <mortenhbirk@gmail.com>
2014-12-06 11:48:53 +0100

    Make communication with server work. Add loads of test butttons

--- app/src/main/java/com/cac/camp/camp/ServerCommunicator.java
+++ app/src/main/java/com/cac/camp/camp/ServerCommunicator.java
@@ -7,15 +7,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Birk on 03-12-2014.
@@ -23,7 +25,7 @@ import java.util.ArrayList;
 public class ServerCommunicator {
 
     private RequestQueue queue;
-    private String url = "http://www.noerskovlund-forsamlingshus.dk/contextaware/handleRequest.php";
+    private String url = "http://www.noerskovlund-forsamlingshus.dk/contextaware/testRequest.php";
 
     public  ServerCommunicator(Context c) {
         queue = Volley.newRequestQueue(c);
@@ -53,6 +55,53 @@ public class ServerCommunicator {
         requestServer(json);
     }
 
+    public void createPlaylist(ArrayList userList, String context) {
+        JSONObject json = new JSONObject();
+        JSONArray users = new JSONArray(userList);
+
+        Log.d("sc", "CreatePlaylist Called");
+        try {
+
+            json.put("type", "createPlaylist");
+            json.put("context", context);
+            json.put("users", users);
+        } catch (JSONException e) {
+            //Handle error in creating
+        }
+        requestServer(json);
+    }
+
+    public void updatePlaylist(ArrayList userList, String context, String playlistID) {
+        JSONObject json = new JSONObject();
+        JSONArray users = new JSONArray(userList);
+
+        Log.d("sc", "UpdatePlaylist Called");
+        try {
+
+            json.put("type", "createPlaylist");
+            json.put("context", context);
+            json.put("users", users);
+            json.put("playlistID", playlistID);
+        } catch (JSONException e) {
+            //Handle error in creating
+        }
+        requestServer(json);
+    }
+
+    public void getPlaylist(String playlistID) {
+        JSONObject json = new JSONObject();
+
+        Log.d("sc", "UpdatePlaylist Called");
+        try {
+
+            json.put("type", "createPlaylist");
+            json.put("playlistID", playlistID);
+        } catch (JSONException e) {
+            //Handle error in creating
+        }
+        requestServer(json);
+    }
+
 
 
     public void requestServer(JSONObject json) {
@@ -64,6 +113,7 @@ public class ServerCommunicator {
                 @Override
                 public void onResponse(JSONObject response) {
                     Log.d("sc", "request worked");
+                    Log.d("sc", response.toString());
                 }
             }, new Response.ErrorListener() {
 
@@ -74,6 +124,9 @@ public class ServerCommunicator {
 
             }
         });
+
+
+
         queue.add(jsObjRequest);
     }
 
