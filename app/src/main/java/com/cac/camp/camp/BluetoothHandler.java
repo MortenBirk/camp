329a6602fcffdd9f223edfea7b848136cd7ebb9b
MortenBirk <mortenhbirk@gmail.com>
2014-12-11 17:04:12 +0100

    Adding classes for bluetooth discovery

---
+++ app/src/main/java/com/cac/camp/camp/BluetoothHandler.java
@@ -0,0 +1,7 @@
+package com.cac.camp.camp;
+
+/**
+ * Created by Birk on 08-12-2014.
+ */
+public class BluetoothHandler {
+}
---
+++ app/src/main/java/com/cac/camp/camp/BluetoothServer.java
@@ -0,0 +1,7 @@
+package com.cac.camp.camp;
+
+/**
+ * Created by Birk on 10-12-2014.
+ */
+public class BluetoothServer {
+}
